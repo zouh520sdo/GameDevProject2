e31e0bf50461a1fca7733ff7e5f0ba9a212ab37f
minpark283 <parkm4@rpi.edu>
2018-10-09 01:42:49 -0400

    Shifting Cards and deletng cards, and moving cards to original location

--- phaser_start_here/js/cards.js
+++ phaser_start_here/js/cards.js
@@ -4,15 +4,42 @@ class Cards extends Phaser.Sprite{
     //constructor
     constructor(game, num, id)
     {
-        console.log(game.cardAreaHeight);
-        super(game,num * 240, gameplayState.cardAreaHeight, "card" + id );
+        
+        super(game,num * 240, 850, "card" + id );
+        this.savedx = num * 240;
+        this.savedy = 850;
+        this.lastx = (num* 240) - 240;
         this.exist = false;
         this.activated = false;
+        this.id = id;
+        this.num = num;
+        this.game.physics.enable(this);
        
+       
+       Cards.prototype.shift = function()
+       {
+           this.body.velocity.x = -600;
+           this.num -= 1;
+
+
+       }
+
+       Cards.prototype.stop = function()
+       {
+           if(this.x <= this.lastx)
+           {
+               this.body.velocity.x = 0;
+               this.savedy -= 240;
+               this.lastx -= 240;
+           }
+           
+       }
+
+    
     }
 
     //effect based on id of the card
-   
+ 
 }
 
 
