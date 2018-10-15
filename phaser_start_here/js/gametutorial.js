de679b6a867192a6e1ed67172618f08e6353f84d
minpark283 <parkm4@rpi.edu>
2018-10-15 03:32:33 -0400

    made few changes to title screen stuff

--- phaser_start_here/js/gametutorial.js
+++ phaser_start_here/js/gametutorial.js
@@ -7,7 +7,12 @@ gametutorial.prototype.create = function() {
 
     game.add.sprite(0,0, "titlescreen");
     
-    
+    backbutton = game.add.button(game.world.centerX - 95, 800, 'star', this.backactionOnClick, this, 2, 1, 0);
     
 
 };
+
+gametutorial.prototype.backactionOnClick = function()
+{
+  game.state.start("Title");
+}
