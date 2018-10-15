dbdc653b4f2d46dc711235c6dba31461b1abd1fd
zouh520sdo <zouh520sdo@gmail.com>
2018-10-15 17:12:41 -0400

    Change probability to 60% 30% 10%

--- phaser_start_here/js/asherahPole.js
+++ phaser_start_here/js/asherahPole.js
@@ -50,13 +50,13 @@ class AsherahPole extends Phaser.Sprite {
                 let rantemp = this.game.rnd.integerInRange(2,4);
                 // random card category
                 let prob = Math.random();
-                if (prob < 0) {
+                if (prob < 0.6) {
                     // Silver card
                     console.log("Get silver card");
                     rantemp = Cards.category.silver[game.rnd.integerInRange(0, Cards.category.silver.length-1)];
                     console.log(rantemp);
                 }
-                else if (prob < 0){
+                else if (prob < 0.9){
                     console.log("Get gold card");
                     rantemp = Cards.category.gold[game.rnd.integerInRange(0, Cards.category.gold.length-1)];
                     console.log(rantemp);
