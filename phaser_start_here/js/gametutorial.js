696a23be1a39be6ef837e92c517f9acfdd1e0501
minpark283 <parkm4@rpi.edu>
2018-10-17 12:05:12 -0400

    change button position

--- phaser_start_here/js/gametutorial.js
+++ phaser_start_here/js/gametutorial.js
@@ -10,8 +10,8 @@ gametutorial.prototype.create = function() {
     game.add.sprite(0,0, "titlescreen");
      this.tutorial = game.add.sprite( 0,0, "tutorial1");
     
- backbutton = game.add.button(game.world.centerX - 800, 800, 'menubutton', this.backactionOnClick, this, 2, 1, 0);
-    moretutorial = game.add.button(game.world.centerX + 500, 800, 'nextbutton', this.moretutorialOnClick, this, 2, 1, 0);
+ backbutton = game.add.button(game.world.centerX- 200 , 1000, 'menubutton', this.backactionOnClick, this, 2, 1, 0);
+    moretutorial = game.add.button(game.world.centerX + 650, 1000, 'nextbutton', this.moretutorialOnClick, this, 2, 1, 0);
     
   
 };
@@ -28,17 +28,15 @@ gametutorial.prototype.moretutorialOnClick = function()
        {
     this.pagenum +=1;
     console.log("tutorial" + this.pagenum);
+       }
     this.tutorial.loadTexture("tutorial" + this.pagenum);
     if(this.pagenum === 2)
         {
-            backtutorial = game.add.button(game.world.centerX , 800, 'backbutton', this.backtutorialOnClick, this, 2, 1, 0);
-        }
-           if(this.pagenum === 3)
-               {
-                    moretutorial.destroy();
-               }
+            backtutorial = game.add.button(game.world.centerX -1000, 1000, 'backbutton', this.backtutorialOnClick, this, 2, 1, 0);
+            moretutorial.destroy();
        }
-}
+    
+};
 
 gametutorial.prototype.backtutorialOnClick = function()
 {
@@ -47,13 +45,11 @@ gametutorial.prototype.backtutorialOnClick = function()
     this.pagenum -=1;
     console.log("tutorial" + this.pagenum);
     this.tutorial.loadTexture("tutorial" + this.pagenum);
-    if(this.pagenum === 2)
-        {
-            moretutorial = game.add.button(game.world.centerX + 500, 800, 'nextbutton', this.moretutorialOnClick, this, 2, 1, 0);
-        }
-           if(this.pagenum === 1)
-               {
-                   backtutorial.destroy();
-               }
        }
-}
+    if(this.pagenum === 1)
+        {
+            moretutorial = game.add.button(game.world.centerX + 650, 1000, 'nextbutton', this.moretutorialOnClick, this, 2, 1, 0);
+            backtutorial.destroy();
+        }
+       
+};
