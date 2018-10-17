9a68d0c658d141c4201f7ac9e2ae1c51c5a7f613
zouh520sdo <zouh520sdo@gmail.com>
2018-10-16 23:57:40 -0400

    Add menu button to the victory menu

--- phaser_start_here/js/gameWin.js
+++ phaser_start_here/js/gameWin.js
@@ -8,8 +8,13 @@ gameWinState.prototype.preload = function () {
 
 gameWinState.prototype.create = function () {
     game.add.sprite(0,0, "victory");
+    playbutton = game.add.button(game.world.centerX - 95, 600, 'menubutton', this.titleactionOnClick, this, 2, 1, 0);
     // Score UI
     this.scoreText = game.add.text(16,16,"You Win!", {fontSize:"32px", fill:"#000000", align:"center"});
+};
+
+gameWinState.prototype.titleactionOnClick = function() {
+     game.state.start("Title");
 };
 
 gameWinState.prototype.update = function () {
